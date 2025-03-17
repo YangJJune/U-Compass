35edde75e762a1cde6317b6261bbacebe3c115d8
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-03-17 23:49:50 +0900

    Repackiging

--- ikseong/compose-navigation/app/src/main/java/com/example/composenavigation/MainActivity.kt
+++ ikseong/compose-navigation/app/src/main/java/com/example/composenavigation/MainActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composenavigation.capsuled.CapsuledNavHost
 import com.example.composenavigation.ui.theme.ComposeNavigationTheme
@@ -34,5 +40,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     ComposeNavigationTheme {
+        Box {
+            Image(bitmap = ImageBitmap(0, 0), contentDescription = "Artist image")
+            Icon(Icons.Filled.Check, contentDescription = "Check mark")
+        }
     }
 }
