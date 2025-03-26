15b0905393019e70caf29ba053b1bed81975fa3b
ikseong00 <iksung1289@konkuk.ac.kr>
2025-03-26 13:10:09 +0900

    feat: 이미지 url 로딩용 remote 기능 구현

--- ikseong/compose-image/app/build.gradle.kts
+++ ikseong/compose-image/app/build.gradle.kts
@@ -1,7 +1,14 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+val properties = Properties().apply {
+    load(project.rootProject.file("local.properties").inputStream())
 }
 
 android {
@@ -16,6 +23,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "BASE_URL", properties["BASE_URL"].toString())
     }
 
     buildTypes {
@@ -36,6 +44,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
---
+++ ikseong/compose-image/app/src/main/java/com/example/composeimage/viewmodel/ImageViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.composeimage.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.composeimage.repository.ImageRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class ImageViewModel : ViewModel() {
+    private val repository = ImageRepository()
+
+    private val _images = MutableStateFlow<List<String>>(emptyList())
+    val images = _images.asStateFlow()
+
+    private val _error = MutableStateFlow<String?>(null)
+    val error = _error.asStateFlow()
+
+    init {
+        getImages()
+    }
+
+    private fun getImages() {
+        viewModelScope.launch {
+            repository.getImages().fold(
+                onSuccess = { response ->
+//                    _images.value = response.response.homeImgs.map { it.homeImgUrl }
+                    val newList = response.response.homeImgs.map { it.homeImgUrl }
+                    _images.value = newList
+                },
+                onFailure = { exception ->
+                    _error.value = exception.message
+                }
+            )
+        }
+    }
+}
