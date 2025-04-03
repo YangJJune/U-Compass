b21d71b95a599c0f2780087431e1724c10f4721b
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-04-04 02:38:10 +0900

    feat: 2회 이상 거절한 경우 앱 설정으로 이동

--- ikseong/compose-permission/app/src/main/java/com/ikseong/composepermission/CameraPermissionBox.kt
+++ ikseong/compose-permission/app/src/main/java/com/ikseong/composepermission/CameraPermissionBox.kt
@@ -1,6 +1,10 @@
 package com.ikseong.composepermission
 
 import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,8 +14,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
@@ -22,10 +31,12 @@ import com.google.accompanist.permissions.shouldShowRationale
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun CameraPermissionBox(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
     // 단일 권한 상태 관리
     val cameraPermissionState = rememberPermissionState(
         permission = Manifest.permission.CAMERA
     )
+    var showRationale by remember { mutableStateOf(false) }
 
     Column(
         modifier = modifier,
@@ -45,7 +56,7 @@ fun CameraPermissionBox(modifier: Modifier = Modifier) {
             }
             // 권한이 거부된 경우 사용자에게 이유 설명
             cameraPermissionState.status.shouldShowRationale -> {
-                Text("카메라 기능을 사용하기 위해서는 카메라 권한이 필요합니다. 권한을 허용해주세요.")
+                Text("[1회 거절] 카메라 기능을 사용하기 위해서는 카메라 권한이 필요합니다. 권한을 허용해주세요.")
                 Spacer(modifier = Modifier.height(8.dp))
                 Button(onClick = {
                     cameraPermissionState.launchPermissionRequest()
@@ -55,12 +66,26 @@ fun CameraPermissionBox(modifier: Modifier = Modifier) {
             }
             // 처음 권한 요청하는 경우
             else -> {
-                Text("이 앱은 카메라 기능을 제공하기 위해 카메라 권한이 필요합니다.")
-                Spacer(modifier = Modifier.height(8.dp))
-                Button(onClick = {
-                    cameraPermissionState.launchPermissionRequest()
-                }) {
-                    Text("권한 요청하기")
+                if (showRationale) {
+                    // 2회 이상 거부한 경우 설정으로 이동
+                    Text("[2회 거절] 권한이 필요합니다. 앱 설정에서 권한을 허용해주세요.")
+                    Button(onClick = {
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                            data = Uri.fromParts("package", context.packageName, null)
+                        }
+                        context.startActivity(intent)
+                    }) {
+                        Text("설정으로 이동")
+                    }
+                } else {
+                    // 최초 요청
+                    Text("최초 요청")
+                    Button(onClick = {
+                        cameraPermissionState.launchPermissionRequest()
+                        showRationale = true
+                    }) {
+                        Text("권한 요청하기")
+                    }
                 }
             }
         }
