ac285b76ff05c9b9250b7b7f7654ecfbfd87a4af
ikseong00 <iksung1289@konkuk.ac.kr>
2025-04-12 22:51:08 +0900

    feat: MainUiAction 사용자 액션 선언 #14

--- app/src/main/java/com/ikseong/ucompass/main/viewmodel/MainUiAction.kt
+++ app/src/main/java/com/ikseong/ucompass/main/viewmodel/MainUiAction.kt
@@ -1,4 +1,8 @@
 package com.ikseong.ucompass.main.viewmodel
 
-interface MainUiAction {
+sealed interface MainUiAction {
+    data object OnProfileClick : MainUiAction
+    data object OnAddressClick : MainUiAction
+    data object OnCreateRoomClick : MainUiAction
+    data class OnRoomClick(val id: Long) : MainUiAction
 }
--- app/src/main/java/com/ikseong/ucompass/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ikseong/ucompass/main/viewmodel/MainViewModel.kt
@@ -1,10 +1,29 @@
 package com.ikseong.ucompass.main.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.ikseong.ucompass.create.viewmodel.CreateUiAction
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(): ViewModel() {
+class MainViewModel @Inject constructor() : ViewModel() {
 
+    private val _uiState = MutableStateFlow(MainUiState())
+    val uiState = _uiState.asStateFlow()
+
+    private val _uiEvent = Channel<MainUiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onCreateUiAction(action: MainUiAction) {
+        when (action) {
+            MainUiAction.OnAddressClick -> TODO()
+            MainUiAction.OnCreateRoomClick -> TODO()
+            MainUiAction.OnProfileClick -> TODO()
+            is MainUiAction.OnRoomClick -> TODO()
+        }
+    }
 }
