f63dc93c10de41fe5a8c098e08b20af51bb19edc
ikseong00 <iksung1289@konkuk.ac.kr>
2025-04-12 22:48:34 +0900

    feat: MainUiState 프로퍼티 선언 #14

--- app/src/main/java/com/ikseong/ucompass/main/viewmodel/MainUiState.kt
+++ app/src/main/java/com/ikseong/ucompass/main/viewmodel/MainUiState.kt
@@ -1,3 +1,16 @@
 package com.ikseong.ucompass.main.viewmodel
 
-data class MainUiState()
+data class MainUiState(
+    val name: String = "",
+    val address: String = "",
+    val profileUrl: String = "",
+    val roomList : List<RoomInfo> = listOf()
+)
+
+data class RoomInfo(
+    val roomId: Long = 0L,
+    val roomName: String = "",
+    val hostName: String = "",
+    val roomLink: String = "",
+    val isHost: Boolean = false
+)
