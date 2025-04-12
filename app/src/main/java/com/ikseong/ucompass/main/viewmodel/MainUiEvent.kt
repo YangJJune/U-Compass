e92b1ad96c38b1e9b03fd75f287dec3c31ba311c
ikseong00 <iksung1289@konkuk.ac.kr>
2025-04-12 22:58:45 +0900

    feat: MainUi 이벤트와 액션 연결 #14

--- app/src/main/java/com/ikseong/ucompass/main/viewmodel/MainUiEvent.kt
+++ app/src/main/java/com/ikseong/ucompass/main/viewmodel/MainUiEvent.kt
@@ -3,6 +3,6 @@ package com.ikseong.ucompass.main.viewmodel
 sealed interface MainUiEvent {
     data object RequestLocationPermissionDialog: MainUiEvent
     data object EditProfileDialog: MainUiEvent
-    data object NavigateToRoom : MainUiEvent
+    data class NavigateToRoom(val id: Long) : MainUiEvent
     data object NavigateToCreateRoom : MainUiEvent
 }
--- app/src/main/java/com/ikseong/ucompass/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ikseong/ucompass/main/viewmodel/MainViewModel.kt
@@ -1,12 +1,13 @@
 package com.ikseong.ucompass.main.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.ikseong.ucompass.create.viewmodel.CreateUiAction
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -23,7 +24,31 @@ class MainViewModel @Inject constructor() : ViewModel() {
             MainUiAction.OnAddressClick -> TODO()
             MainUiAction.OnCreateRoomClick -> TODO()
             MainUiAction.OnProfileClick -> TODO()
-            is MainUiAction.OnRoomClick -> TODO()
+            is MainUiAction.OnRoomClick -> navigateToRoom(action.id)
+        }
+    }
+
+    private fun showLocationPermissionDialog() {
+        viewModelScope.launch {
+            _uiEvent.send(MainUiEvent.RequestLocationPermissionDialog)
+        }
+    }
+
+    private fun navigateToCreateRoom() {
+        viewModelScope.launch {
+            _uiEvent.send(MainUiEvent.NavigateToCreateRoom)
+        }
+    }
+
+    private fun navigateToRoom(id: Long) {
+        viewModelScope.launch {
+            _uiEvent.send(MainUiEvent.NavigateToRoom(id))
+        }
+    }
+
+    private fun showEditProfileDialog() {
+        viewModelScope.launch {
+            _uiEvent.send(MainUiEvent.EditProfileDialog)
         }
     }
 }
