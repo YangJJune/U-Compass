25eeb311a63dd0d050c240aab847975cada7da3f
ikseong00 <iksung1289@konkuk.ac.kr>
2025-04-18 13:53:48 +0900

    feat: 메인 화면 UI 구현 완료 #14

--- app/src/main/java/com/ikseong/ucompass/main/screen/MainScreen.kt
+++ app/src/main/java/com/ikseong/ucompass/main/screen/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.ikseong.ucompass.main.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -22,12 +23,15 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ikseong.ucompass.common.component.ObserveAsEvents
 import com.ikseong.ucompass.common.component.UCompassButton
 import com.ikseong.ucompass.common.component.UCompassLogo
+import com.ikseong.ucompass.main.component.MainRoomList
 import com.ikseong.ucompass.main.component.MainUserContent
 import com.ikseong.ucompass.main.viewmodel.MainUiAction
 import com.ikseong.ucompass.main.viewmodel.MainUiEvent
 import com.ikseong.ucompass.main.viewmodel.MainUiState
 import com.ikseong.ucompass.main.viewmodel.MainViewModel
+import com.ikseong.ucompass.main.viewmodel.RoomInfo
 import com.ikseong.ucompass.ui.theme.UCompassTheme.typography
+import kotlinx.collections.immutable.persistentListOf
 
 
 @Composable
@@ -88,11 +92,22 @@ fun MainScreen(
             )
 
             Text(
-                modifier = Modifier.padding(start = 21.dp),
+                modifier = Modifier.padding(start = 21.dp, top = 48.dp),
                 text = "참여한 컴패스 리스트",
                 style = typography.semiBold.copy(
                     fontSize = 24.sp,
                 )
+            )
+
+            MainRoomList(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 21.dp, vertical = 32.dp),
+                roomList = uiState.roomList,
+                onRoomClick = { onAction(MainUiAction.OnRoomClick(it)) },
+                onActionClick = { roomInfo, isHost ->
+                    onAction(MainUiAction.OnRoomActionClick(roomInfo, isHost))
+                }
             )
 
         }
@@ -100,6 +115,7 @@ fun MainScreen(
         UCompassButton(
             modifier = Modifier
                 .fillMaxWidth()
+                .background(Color(0xFFF8FFFD))
                 .padding(horizontal = 22.dp, vertical = 15.dp)
                 .height(62.dp)
                 .align(Alignment.BottomCenter),
@@ -115,7 +131,33 @@ fun MainScreen(
 private fun MainScreenPreview() {
     MainScreen(
         padding = PaddingValues(0.dp),
-        uiState = MainUiState(),
+        uiState = MainUiState(
+            name = "김익성",
+            address = "서울특별시 강남구 역삼동",
+            profileUrl = "",
+            roomList = persistentListOf(
+                RoomInfo(
+                    hostName = "HostName1",
+                    roomLink = "RoomLink1",
+                    roomName = "RoomName1"
+                ),
+                RoomInfo(
+                    hostName = "HostName2",
+                    roomLink = "RoomLink2",
+                    roomName = "RoomName2"
+                ),
+                RoomInfo(
+                    hostName = "HostName3",
+                    roomLink = "RoomLink3",
+                    roomName = "RoomName3"
+                ),
+                RoomInfo(
+                    hostName = "HostName4",
+                    roomLink = "RoomLink4",
+                    roomName = "RoomName4"
+                )
+            )
+        ),
         onAction = {}
     )
 }
