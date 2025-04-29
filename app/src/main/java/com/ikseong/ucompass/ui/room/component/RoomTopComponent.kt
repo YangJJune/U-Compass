5adc42c46a9a8b3613c5b657a400bb10939e6596
ikseong00 <iksung1289@konkuk.ac.kr>
2025-04-29 21:23:02 +0900

    feat: 방 화면 상단 컴포넌트 구현 #17

---
+++ app/src/main/java/com/ikseong/ucompass/ui/room/component/RoomTopComponent.kt
@@ -0,0 +1,66 @@
+package com.ikseong.ucompass.ui.room.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ikseong.ucompass.R
+import com.ikseong.ucompass.ui.theme.UCompassTheme.typography
+
+@Composable
+fun RoomTopComponent(
+    modifier: Modifier = Modifier,
+    onBackClick: () -> Unit,
+    onDeleteClick: () -> Unit,
+    roomName: String,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+            .padding(top = 32.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = onBackClick) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_arrow_left_fill_36),
+                contentDescription = "Back Icon",
+                tint = Color(0xFF606060)
+            )
+        }
+        Text(
+            text = roomName,
+            style = typography.semiBold.copy(
+                fontSize = 24.sp,
+            )
+        )
+
+        IconButton(onClick = onDeleteClick) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_room_delete_36),
+                contentDescription = "Delete Room Icon",
+                tint = Color(0xFF606060)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun RoomTopComponentPreview() {
+    RoomTopComponent(
+        onBackClick = {},
+        onDeleteClick = {},
+        roomName = "위치 찾기 방 1"
+    )
+}
