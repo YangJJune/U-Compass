3b8aafcb8ddb0e6e3fbf06be593a2b0137e1e1f5
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-05-31 23:47:53 +0900

    refactor: 참가자 위치 핀 틴트 오류 수정 #36

--- app/src/main/java/com/ikseong/ucompass/ui/room/component/ParticipantPin.kt
+++ app/src/main/java/com/ikseong/ucompass/ui/room/component/ParticipantPin.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import com.ikseong.ucompass.R
 import com.ikseong.ucompass.ui.model.Direction
 import com.ikseong.ucompass.ui.model.DistanceType
 import com.ikseong.ucompass.ui.theme.UCompassTheme.typography
@@ -23,7 +22,6 @@ fun ParticipantPin(
     direction: Direction,
     distance: Int,
     type: DistanceType,
-    isMapVisible: Boolean
 ) {
     Column(
         modifier = modifier,
@@ -32,30 +30,34 @@ fun ParticipantPin(
             text = name,
             style = typography.medium.copy(
                 fontSize = 14.sp,
-                color = if (isMapVisible) Color(0xFF606060) else Color.White
+                color = Color.White
             )
         )
         Text(
             text = "${direction.name} ${distance}M",
             style = typography.medium.copy(
                 fontSize = 14.sp,
-                color = if (isMapVisible) Color(0xFF606060) else Color.White
+                color = Color.White
             )
         )
-
         Icon(
             modifier = Modifier
                 .rotate(direction.rotation)
                 .size(type.size),
-            painter = painterResource(id = R.drawable.ic_participant_pin),
+            painter = painterResource(id = type.iconRes),
             contentDescription = "Participant Pin",
-            tint = type.color
+            tint = Color.Unspecified
         )
     }
 }
 
-@Preview
+@Preview(widthDp = 200, heightDp = 200)
 @Composable
 private fun ParticipantPinPreview() {
-//    ParticipantPin()
+    ParticipantPin(
+        name = "홍길동",
+        direction = Direction.N,
+        distance = 500,
+        type = DistanceType.ZERO,
+    )
 }
